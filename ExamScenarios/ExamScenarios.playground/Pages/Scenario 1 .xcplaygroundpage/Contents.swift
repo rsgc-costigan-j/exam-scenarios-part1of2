59bbb2214edf6d9aa36d79ecc60253bc4dd939d1
Kathleen McLaughlin <kathleen.mclaughlin.17@gmail.com>
2017-05-21 11:30:43 -0400

    working on colours

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 1 .xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 1 .xcplaygroundpage/Contents.swift
@@ -35,27 +35,32 @@ canvas.drawAxes()
 var h = 0
 var s = 0
 var b = 0
-for y in stride(from: 60, to: -300, by: -99){
+var c = 1
 
-for x in stride(from: 250, to: -100, by: -99){
-    canvas.fillColor = Color(hue: h, saturation: s, brightness: b, alpha: 100)
-    canvas.drawRectangle(centreX: x, centreY: y, width: 100, height: 100)
+for y in stride(from: 60, to: -300, by: -99){
     
-    if x == 250 {
-        h = 21
-        s = 89
-        b = 22
-    } else if x == 151{
-        h = 21
-        s = 90
-        b = 47
-    } else if x == 52{
-        h = 21
-        s = 90
-        b = 70
+    for x in stride(from: 250, to: -100, by: -99){
+        canvas.fillColor = Color(hue: h, saturation: s, brightness: b, alpha: 100)
+        canvas.drawRectangle(centreX: x, centreY: y, width: 100, height: 100)
+        
+        if c == 1 || c == 4  || c == 10{
+            h = 21
+            s = 89
+            b = 22
+        } else if c == 2 || c == 5 || c == 7 || c == 10 || c == 8{
+            s = 90
+            b = 47
+        } else if c == 3 || c == 6 || c == 9 {
+            s = 90
+            b = 70
+        }
+        c += 1
+        
+    }
 }
-}
-}
+
+
+
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
